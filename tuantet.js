8a9eb5adbc97d75789dd0c84cc43c78f30ebc8b9
thienkieunu6-lab <thienkieunu6@gmail.com>
2026-02-27 16:10:28 +0000

    commit all pages

--- tuantet.js
+++
@@ -1,4 +0,0 @@
-document.querySelector('.contact-form')?.addEventListener('submit', (event) => {
-  event.preventDefault();
-  alert('Cảm ơn bạn đã đóng góp ý kiến cho website Tết quê hương!');
-});
